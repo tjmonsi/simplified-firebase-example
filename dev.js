0d9b9f7af33f0f9d349fc311a79a378ead1b4eaa
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-12-08 08:46:15 +0800

    add parts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ dev.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.writeFileSync('./public/sw.js', `
+console.log(\`Development mode. Will cache files in production mode. Generated: \${new Date().toString()}\`);
+// The install handler takes care of precaching the resources we always need.
+this.addEventListener('install', event => {
+  if (this.skipWaiting) { this.skipWaiting(); }
+});
+// The activate handler takes care of cleaning up old caches.
+this.addEventListener('activate', event => {
+  console.log('activated');
+});
+`, 'utf8');
