9374cdd5d2f4b641574837dc65dc1f94fd02a5d7
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-12-08 09:17:35 +0800

    fixed rollup
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 module.exports = {
   input: 'src/index.js',
   output: {
     file: 'public/index.js',
-    format: 'esm'
-  }
+    format: 'umd'
+  },
+  plugins: [
+    resolve(), // so Rollup can find `ms`
+    commonjs()
+  ]
 };
