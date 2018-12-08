48d2ae05edd02073ec2f6fd0628ffc8d1f68a19f
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-12-08 12:28:01 +0800

    changed functions
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,1 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-const database = admin.database();
-
-exports.onTodoCreate = functions.database.ref('/todo/data/{uid}/{todoId}').onCreate((snap, context) => {
-  const { params } = context;
-  const { uid } = params;
-  const data = snap.val();
-  const updates = {};
-  updates[`status/data/${uid}`] = data.todo;
-  return database.ref().update(updates);
-});
